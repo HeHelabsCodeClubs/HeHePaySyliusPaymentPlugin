67752e800a5387b6f58231cd33dd1651e1363405
Kevin Kayisire <kaykevin1@gmail.com>
2021-03-22 11:10:11 +0200

    bug fix

--- src/Payum/Action/CaptureAction.php
+++ src/Payum/Action/CaptureAction.php
@@ -21,6 +21,10 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
     /** @var SyliusApi */
     private $api;
 
+    // public function __constructor(Client $client) {
+    //     $this->client = $client;
+    // }
+
     public function get_token() {
         $url = "https://gateway.hehepay.rw/api/v1/auth/get-token";
         
@@ -51,7 +55,7 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
         return $result;
     }
 
-    public function execute(Client $client, $request): void
+    public function execute($request): void
     {
         RequestNotSupportedException::assertSupports($this, $request);
 
@@ -61,7 +65,7 @@ final class CaptureAction implements ActionInterface, ApiAwareInterface
         try {
             $token = $this->get_token();
 
-            $response = $client->request('POST', 'https://gateway.hehepay.rw/api/v1/payments/request', [
+            $response = $this->client->request('POST', 'https://gateway.hehepay.rw/api/v1/payments/request', [
                 'body' => json_encode([
                     'order_id' => $payment->getOrder(),
                     'amount' => $payment->getAmount(),
